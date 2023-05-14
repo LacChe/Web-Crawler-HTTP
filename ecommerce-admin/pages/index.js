f7555b48de1a13b5011e832f7f314e1b01c05bac
LacChe <lacche96@gmail.com>
2023-05-14 23:00:23 +0800

    nav stub

--- ecommerce-admin/pages/index.js
+++ ecommerce-admin/pages/index.js
@@ -1,38 +1,38 @@
+import Nav from '@/components/nav';
 import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function Home() {
   const user = useUser();
 
-  return (
-    <div className='bg-blue-900 w-screen h-screen flex items-center'>
-      <div className='items-center w-full flex flex-col gap-2'>
-        {user.isLoading ? (
-          <div>Loading...</div>
-        ) : (
-          <>
-            {user.user ? (
-              <div>
-                <div>Signed in as {user.user.name}<br/></div>
-                <a 
-                  className='w-1/4 bg-white p-2 rounded-lg'
-                  href="/api/auth/logout"
-                >
-                  Logout
-                </a>
-              </div>
-            ) : (
-              <div>
-                <a 
-                  className='w-1/4 bg-white p-2 rounded-lg'
-                  href="/api/auth/login"
-                >
-                  Login
-                </a>
-              </div>
-            )}
-          </>
-        )}
+  if(user.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  console.log(user)
+
+  if(user.user) {
+    return (
+      <div className='bg-blue-900 min-h-screen'>
+        <Nav />
+        <div>Signed in as {user.user.name}<br/></div>
+        <a 
+          className='w-1/4 bg-white p-2 rounded-lg'
+          href="/api/auth/logout"
+        >
+          Logout
+        </a>
       </div>
+    )
+  }
+
+  return (
+    <div className='bg-blue-900 w-screen h-screen flex text-center justify-center items-center'>
+      <a 
+        className='w-1/4 bg-white p-2 rounded-lg'
+        href="/api/auth/login"
+      >
+        Login
+      </a>
     </div>
   )
 }
