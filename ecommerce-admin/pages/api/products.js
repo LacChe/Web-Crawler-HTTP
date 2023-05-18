713fa2b8fd61ddeae5497342b7c136c35dd0270c
LacChe <lacche96@gmail.com>
2023-05-18 12:45:12 +0800

    product edit delete pages

--- ecommerce-admin/pages/api/products.js
+++ ecommerce-admin/pages/api/products.js
@@ -6,11 +6,32 @@ export default async function handler(req, res) {
     
     await mongooseConnect();
 
+    if(method === 'GET'){
+        if(req.query?.id) {
+            res.json(await Product.findOne({_id:req.query.id}));
+        } else {
+            res.json(await Product.find());
+        }
+    }
+
     if(method === 'POST'){
-        const {title, desciption, price} = req.body;
+        const {title, description, price} = req.body;
         const productDoc = await Product.create({
-            title, desciption, price
+            title, description, price
         })
         res.json(productDoc);
+    }
+
+    if(method === 'PUT'){
+        const {_id, title, description, price} = req.body;
+        await Product.updateOne({_id}, {title, description, price});
+        res.json(true);
+    }
+
+    if(method === 'DELETE'){
+        if(req.query?.id) {
+            await Product.deleteOne({_id:req.query.id});
+        }
+        res.json(true);
     }
 }
--- ecommerce-admin/pages/products/new.js
+++ ecommerce-admin/pages/products/new.js
@@ -1,42 +1,11 @@
-import Layout from '@/components/Layout'
-import React, { useState } from 'react'
-
-import axios from 'axios';
-import { useRouter } from 'next/router';
+import Layout from '@/components/Layout';
+import ProductForm from '@/components/ProductForm';
 
 const NewProduct = () => {
-
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [price, setPrice] = useState(0);
-    const [goToProducts, setGoToProducts] = useState(false);
-
-    const router = useRouter();
-
-    async function createProduct(e) {
-        e.preventDefault();
-        const data ={title, description, price};
-        await axios.post('/api/products', data);
-        setGoToProducts(true);
-    }
-
-    if(goToProducts){
-        router.push('/products');
-    }
-
-    
   return (
     <Layout>
-        <form onSubmit={createProduct}>
-            <h1>New Product</h1>
-            <label>Product Name</label>
-            <input type='text' placeholder='product name' onChange={e => setTitle(e.target.value)} value={title}/>
-            <label>Description</label>
-            <textarea placeholder='description'onChange={e => setDescription(e.target.value)} value={description}/>
-            <label>Price (USD)</label>
-            <input type='number' placeholder='price'onChange={e => setPrice(e.target.value)} value={price}/>
-            <button type='submit' className='btn-primary'>Save</button>
-        </form>
+        <h1>New Product</h1>
+        <ProductForm/>
     </Layout>
   )
 }
