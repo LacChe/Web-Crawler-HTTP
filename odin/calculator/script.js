304b6c5014be0eb38e74a8992a01101d159ab02a
LacChe <lacche96@gmail.com>
2023-06-05 18:37:00 +0800

    odin calculator added ui

---
+++ odin/calculator/script.js
@@ -0,0 +1,89 @@
+var displayDiv = document.querySelector('#display');
+
+//displayDiv.textContent = '';
+
+var button0 = document.querySelector('#btn0');
+var button1 = document.querySelector('#btn1');
+var button2 = document.querySelector('#btn2');
+var button3 = document.querySelector('#btn3');
+var button4 = document.querySelector('#btn4');
+var button5 = document.querySelector('#btn5');
+var button6 = document.querySelector('#btn6');
+var button7 = document.querySelector('#btn7');
+var button8 = document.querySelector('#btn8');
+var button9 = document.querySelector('#btn9');
+var buttonAdd = document.querySelector('#add');
+var buttonMinus = document.querySelector('#minus');
+var buttonMult = document.querySelector('#mult');
+var buttonDiv = document.querySelector('#div');
+var buttonDec = document.querySelector('#dec');
+var buttonClear = document.querySelector('#clear');
+var buttonEquals = document.querySelector('#equals');
+
+button0.addEventListener('click', () => {
+    console.log('0');
+})
+
+button1.addEventListener('click', () => {
+    console.log('1');
+})
+
+button2.addEventListener('click', () => {
+    console.log('2');
+})
+
+button3.addEventListener('click', () => {
+    console.log('3');
+})
+
+button4.addEventListener('click', () => {
+    console.log('4');
+})
+
+button5.addEventListener('click', () => {
+    console.log('5');
+})
+
+button6.addEventListener('click', () => {
+    console.log('6');
+})
+
+button7.addEventListener('click', () => {
+    console.log('7');
+})
+
+button8.addEventListener('click', () => {
+    console.log('8');
+})
+
+button9.addEventListener('click', () => {
+    console.log('9');
+})
+
+buttonAdd.addEventListener('click', () => {
+    console.log('add');
+})
+
+buttonMinus.addEventListener('click', () => {
+    console.log('minus');
+})
+
+buttonMult.addEventListener('click', () => {
+    console.log('mult');
+})
+
+buttonDiv.addEventListener('click', () => {
+    console.log('div');
+})
+
+buttonDec.addEventListener('click', () => {
+    console.log('dec');
+})
+
+buttonClear.addEventListener('click', () => {
+    console.log('clear');
+})
+
+buttonEquals.addEventListener('click', () => {
+    console.log('eq');
+})
