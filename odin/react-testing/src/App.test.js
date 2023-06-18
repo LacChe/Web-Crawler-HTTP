cef8acd467e5323321202918f9ccbd1098ccc2e1
LacChe <lacche96@gmail.com>
2023-06-18 13:36:38 +0800

    odin react-testing

--- odin/react-testing/src/App.test.js
+++ odin/react-testing/src/App.test.js
@@ -1,26 +1,46 @@
-// App.test.js
+// FavoriteInput.test.js
 
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
-import App from "./App";
-import { act } from 'react-dom/test-utils';
+import FavoriteInput from "./App";
 
-describe("App component", () => {
-  it("renders magnificent monkeys", () => {
-    // since screen does not have the container property, we'll destructure render to obtain container for this test
-    const { container } = render(<App />);
-    expect(container).toMatchSnapshot();
-  });
-
-  it("renders radical rhinos after button click", async () => {
+describe("Favorite Input", () => {
+  it("calls onChange correct number of times", async () => {
+    const onChangeMock = jest.fn();
     const user = userEvent.setup();
 
-    render(<App />);
-    const button = screen.getByRole("button", { name: "Click Me" });
+    render(<FavoriteInput onChange={onChangeMock} />);
+    const input = screen.getByRole("textbox");
 
-    await user.click(button);
+    await user.type(input, "Lion");
 
-    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
+    expect(onChangeMock).toHaveBeenCalledTimes(4);
+  });
+
+  it("calls onChange with correct argument(s) on each input", async () => {
+    const onChangeMock = jest.fn();
+    const user = userEvent.setup();
+
+    render(<FavoriteInput onChange={onChangeMock} />);
+    const input = screen.getByRole("textbox");
+
+    await user.type(input, "Ox");
+
+    expect(onChangeMock).toHaveBeenNthCalledWith(1, "O");
+    expect(onChangeMock).toHaveBeenNthCalledWith(2, "Ox");
+  });
+
+  it("input has correct values", async () => {
+    const onChangeMock = jest.fn();
+    const user = userEvent.setup();
+
+    render(<FavoriteInput onChange={onChangeMock} />);
+    const input = screen.getByRole("textbox");
+
+    await user.type(input, "Whale");
+
+    expect(input).toHaveValue("Whale");
   });
 });
--- odin/react-testing/src/index.js
+++ odin/react-testing/src/index.js
@@ -6,6 +6,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App onChange={onInputChange} id={0}/>
   </React.StrictMode>
 );
+
+function onInputChange(e){
+  console.log(e);
+}
