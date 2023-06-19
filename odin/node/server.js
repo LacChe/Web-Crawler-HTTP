343f71ed5b7fc0e37218cfb2d9e3c0a74f38d074
LacChe <lacche96@gmail.com>
2023-06-19 19:16:49 +0800

    odin node

--- odin/node/server.js
+++ odin/node/server.js
@@ -1,14 +1,22 @@
-const http = require('http');
+const https = require('https');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const options = {
+  hostname: 'example.com',
+  port: 443,
+  path: '/todos',
+  method: 'GET',
+};
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on('data', d => {
+    process.stdout.write(d);
+  });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+req.on('error', error => {
+  console.error(error);
 });
+
+req.end();
