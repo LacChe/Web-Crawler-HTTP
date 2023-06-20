3234f2b17bf6a30f0ba19ece070da23cdf0f5133
LacChe <lacche96@gmail.com>
2023-06-20 20:48:17 +0800

    odin express mini-message-board

--- odin/express-mini-message-board/routes/index.js
+++ odin/express-mini-message-board/routes/index.js
@@ -1,31 +1,37 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config()
+const mongoose = require("mongoose");
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+const Message = require("../models/message");
+
+let messages = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Messageboard', messages });
+
+  // Set up mongoose connection
+  mongoose.set("strictQuery", false);
+  const mongoDB = process.env.MONGO_DB_STRING;
+
+  main().catch((err) => console.log(err));
+  async function main() {
+    await mongoose.connect(mongoDB);
+    messages = await Message.find();
+    res.render('index', { title: 'Mini Messageboard', messages });
+  }
+
 });
 
 router.get('/new', function(req, res, next) {
   res.render('form');
 });
 
-router.post('/new', function(req, res, next) {
+router.post('/new', async function(req, res, next) {
+  const msg = new Message({message: req.body.message, name: req.body.name, date: new Date()})
+  await msg.save();
   messages.push({text: req.body.message, user: req.body.name, added: new Date()});
-  console.log(messages);
+  console.log('saved', msg);
   res.redirect('/');
 });
 
