5726d907f78e9eb81ecc89173e83a49a3f5dd60a
LacChe <lacche96@gmail.com>
2023-06-20 15:02:37 +0800

    odin express local-library

--- odin/express-local-library/populatedb.js
+++ odin/express-local-library/populatedb.js
@@ -5,7 +5,7 @@ async function main(){
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://lacche96:92wnrj6u@local-library.iuyawuw.mongodb.net/?retryWrites=true&w=majority";
+    const uri = process.env.MONGO_DB_STRING;
  
 
     const client = new MongoClient(uri);
@@ -17,7 +17,7 @@ async function main(){
         console.log('b')
  
         // Make the appropriate DB calls
-        await  listDatabases(client);
+        await listDatabases(client);
  
     } catch (e) {
         console.error(e);
