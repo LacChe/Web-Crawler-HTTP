43972a384bf7d5912b4c8efea303ea91fdb0d755
LacChe <lacche96@gmail.com>
2023-06-20 12:35:24 +0800

    odin express intro

--- odin/express-intro/index.js
+++ odin/express-intro/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.send(fs.readFileSync('./index.html', 'utf8'));
+});
+
+app.get("/about", function (req, res) {
+  res.send(fs.readFileSync('./about.html', 'utf8'));
+});
+
+app.get("/", function (req, res) {
+  res.send(fs.readFileSync('./contact-me.html', 'utf8'));
 });
 
 app.listen(port, function () {
