2e8f6fa8879f992b9a953731a0d6b6ead35d7a42
LacChe <lacche96@gmail.com>
2023-06-21 09:02:13 +0800

    odin express local-library

--- odin/express-local-library/app.js
+++ odin/express-local-library/app.js
@@ -9,6 +9,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
+var catalogRouter = require('./routes/catalog');
 
 var app = express();
 
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
