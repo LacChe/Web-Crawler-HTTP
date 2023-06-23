94fe7cdf7b4bb8d1271ca87a52147ba3701e401a
LacChe <lacche96@gmail.com>
2023-06-23 14:39:31 +0800

    odin express rest

---
+++ odin/api-basics/src/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+
+import models from './models/index.js';
+import routes from './routes/index.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    req.context = {
+        models,
+        me: models.users[1],
+    };
+    next();
+});
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
